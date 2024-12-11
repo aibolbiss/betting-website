bb3062ccdd45e81e073cd61a9d3719476e079f1d
Aibol Bissenkul <aibol.biss@gmail.com>
2024-12-11 18:15:49 +0500

    fix project

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,5 @@
 import Block1 from './Block1';
-import Block2 from './Block2';
+// import Block2 from './Block2';
 import Block3 from './Block3';
 import Block4 from './Block4';
 import Block5 from './Block5';
@@ -9,7 +9,7 @@ const MainPage = () => {
   return (
     <div className='container'>
       <Block1 />
-      <Block2 />
+      {/* <Block2 /> */}
       <Block3 />
       <Block4 />
       <Block5 />
